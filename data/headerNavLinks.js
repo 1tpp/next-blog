0f7155c5547a2590c7c1fedb718307dd7b382753
1tpp <it.dang27@gmail.com>
2021-08-16 16:06:34 +0700

    chore: remove tags header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,3 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-]
+const headerNavLinks = [{ href: '/blog', title: 'Blog' }]
 
 export default headerNavLinks
