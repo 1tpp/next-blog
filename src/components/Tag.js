a5ea5f4eb0e7ee3229123358223a5163c281711f
1tpp <it.dang27@gmail.com>
2021-08-16 16:03:50 +0700

    chore: change style css

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mt-2 mr-2 p-1 text-sm font-medium uppercase bg-gray-700 rounded-md shadow-md text-gray-200 hover:bg-gray-800 hover:text-gray-200 dark:hover:text-primary-400 ">
         {text.split(' ').join('-')}
       </a>
     </Link>
